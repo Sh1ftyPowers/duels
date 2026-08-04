fcf92d59802e129a43d42489f4a0e8b58ec57d2f
Sh1ftyPowers <alexlevshoff@gmail.com>
2026-08-04 15:15:54 +0300

    Static Hashes renamed

--- Assets/_Project/Scripts/Units/UnitAnimationManager.cs
+++ Assets/_Project/Scripts/Units/UnitAnimationManager.cs
@@ -6,23 +6,23 @@ namespace Duels.Units
     {
         [SerializeField] private Animator _animator;
 
-        private static readonly int _attackTrigger = Animator.StringToHash("attack");
-        private static readonly int _deathTrigger = Animator.StringToHash("isDead");
-        private static readonly int _winTrigger = Animator.StringToHash("isWinner");
+        private static readonly int AttackTrigger = Animator.StringToHash("attack");
+        private static readonly int DeathTrigger = Animator.StringToHash("isDead");
+        private static readonly int WinTrigger = Animator.StringToHash("isWinner");
 
         public void PlayAttackAnimation()
         {
-            _animator.SetTrigger(_attackTrigger);
+            _animator.SetTrigger(AttackTrigger);
         }
 
         public void PlayDeathAnimation()
         {
-            _animator.SetTrigger(_deathTrigger);
+            _animator.SetTrigger(DeathTrigger);
         }
 
         public void PlayVictoryAnimation()
         {
-            _animator.SetTrigger(_winTrigger);
+            _animator.SetTrigger(WinTrigger);
         }
     }
 }
